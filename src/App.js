97d9924dddb4071b747d30e5d38e856d7b9813c7
iamhosto10 <gerav0400@gmail.com>
2022-08-25 16:57:28 -0500

    fallos

--- src/App.js
+++ src/App.js
@@ -5,39 +5,49 @@ import DataTable from "react-data-table-component";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [columnas, setColumnas] = useState([]);
+  let [inputValue, setInputValue] = useState("");
 
-  const URL = "https://gorest.co.in/public/v2/users";
   const showdata = async (pagina) => {
     console.log(pagina);
     const response = await fetch(pagina);
     const data = await response.json();
     setUsers(data);
+    console.log(data);
+    // data.map((prueba, index) => console.log(index));
+    data.forEach((element) => {
+      element.forEach((ele) => {
+        console.log(ele.id);
+      });
+    });
+    let lista = [];
+
+    for (var key in data) {
+      lista.push(data[key]);
+      console.log(lista);
+    }
+    setColumnas(lista);
   };
 
   // useEffect(() => {
   //   showdata();
   // }, []);
 
-  const columns = [
-    {
-      name: "ID",
-      selector: (row) => row.id,
-    },
-    {
-      name: "name",
-      selector: (row) => row.name,
-    },
-    {
-      name: "email",
-      selector: (row) => row.email,
-    },
-  ];
+  // const columns = [
+  //   {
+  //     name: "ID",
+  //     selector: (row) => row.id,
+  //   },
+  //   {
+  //     name: "name",
+  //     selector: (row) => row.name,
+  //   },
+  //   {
+  //     name: "email",
+  //     selector: (row) => row.email,
+  //   },
+  // ];
 
-  let [inputValue, setInputValue] = useState("");
-
-  const prueba = () => {
-    console.log(inputValue);
-  };
   return (
     <div className="App">
       <input
@@ -47,8 +57,7 @@ function App() {
         onChange={(event) => setInputValue(event.target.value)}
       />
       <button onClick={() => showdata(inputValue)}> boton</button>
-
-      <DataTable columns={columns} data={users} />
+      <DataTable columns={columnas} data={users} />
     </div>
   );
 }
